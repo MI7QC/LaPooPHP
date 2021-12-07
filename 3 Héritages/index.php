a5464032b081bceae52903526ce797bd26be8a8f
sam <samuel.st.jean@gmail.com>
2021-12-06 22:14:08 -0500

    update pdf done

--- 3 Héritages/index.php
+++ 3 Héritages/index.php
@@ -3,14 +3,17 @@ require_once 'classes/Compte.php';
 require_once 'classes/CompteCourant.php';
 require_once 'classes/CompteEpargne.php';
 
-// // Première instance de la classe Compte
+// Instanciation de CompteCourant. 
 $compte1 = new CompteCourant('Sam', 500, 200);
+// call methode retirer de la class comptecourant  solde 500 - 15 = 485.  
+// A tester retiré 600 - 500 = solde insufisant.
 $compte1->retirer(15);
 $compte1->getDecouvert();
 var_dump($compte1);
 
 
 $compte2 = new CompteEpargne('bob', 200, 1);
+// methode retirer de  la class Compte
 $compte2->retirer(15);
 echo 'taux intéret ' . $compte2->getTauxInterets();
 var_dump($compte2);
